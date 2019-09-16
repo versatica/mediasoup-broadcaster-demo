bb97b5f1e394b05e8fa4331226bc100fd7f886c6
Iñaki Baz Castillo <ibc@aliax.net>
2019-09-16 15:02:53 +0200

    Update scripts

--- scripts/get_dep.sh
+++ scripts/get_dep.sh
@@ -6,7 +6,7 @@ PROJECT_PWD=${PWD}
 DEP=$1
 
 current_dir_name=${PROJECT_PWD##*/}
-if [ "${current_dir_name}" != "mediasoup-broadcaster-demo" ] ; then
+if [ "${current_dir_name}" != "mediasoup-broadcaster-demo" ] && [ "${current_dir_name}" != "v3-mediasoup-broadcaster-demo" ] ; then
 	echo ">>> [ERROR] $(basename $0) must be called from broadcaster/ root directory" >&2
 	exit 1
 fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,19 +5,20 @@ set -e
 PROJECT_PWD=${PWD}
 
 current_dir_name=${PROJECT_PWD##*/}
-if [ "${current_dir_name}" != "broadcaster" ] ; then
+	if [ "${current_dir_name}" != "mediasoup-broadcaster-demo" ] && [ "${current_dir_name}" != "v3-mediasoup-broadcaster-demo" ] ; then
 	echo ">>> [ERROR] $(basename $0) must be called from broadcaster/ root directory" >&2
 	exit 1
 fi
 
 if [ "$1" == "build" ]; then
-	# Rebuild.
+	echo ">>> building..."
+	cmake --build build
+elif [ "$1" == "rebuild" ]; then
+	echo ">>> rebuilding..."
 	rm -rf build/
 	cmake . -Bbuild
+	cmake --build build
 fi
-
-# Compile.
-cmake --build build
 
 # Run.
 ./build/broadcaster $@
