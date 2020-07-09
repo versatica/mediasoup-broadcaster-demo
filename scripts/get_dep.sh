bf0e06790cb86a473d2c03fa0e070422c71aead2
José Luis Millán <jmillan@aliax.net>
2020-07-09 08:05:56 +0200

    update deps

--- scripts/get_dep.sh
+++ scripts/get_dep.sh
@@ -45,8 +45,8 @@ function get_dep()
 
 function get_libmediasoupclient()
 {
-	GIT_REPO="https://github.com/copiltembel/libmediasoupclient.git"
-	GIT_TAG="v3-DataChannelsImpl"
+	GIT_REPO="https://github.com/versatica/libmediasoupclient.git"
+	GIT_TAG="v3"
 	DEST="deps/libmediasoupclient"
 
 	get_dep "${GIT_REPO}" "${GIT_TAG}" "${DEST}"
